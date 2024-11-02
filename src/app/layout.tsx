f803f56308017e735758708974efec7b58780721
hyunyikim <hailey@mass-adoption.com>
2024-11-02 18:12:25 +0100

    feat: add pretendard font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,31 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
+const pretendard = localFont({
+  src: [
+    {
+      path: './fonts/Pretendard-Light.subset.woff2',
+      weight: '300',
+    },
+    {
+      path: './fonts/Pretendard-Regular.subset.woff2',
+      weight: '400',
+    },
+    {
+      path: './fonts/Pretendard-Medium.subset.woff2',
+      weight: '500',
+    },
+    {
+      path: './fonts/Pretendard-SemiBold.subset.woff2',
+      weight: '600',
+    },
+    {
+      path: './fonts/Pretendard-Bold.subset.woff2',
+      weight: '700',
+    },
+  ],
+  variable: '--font-pretendard',
+  weight: '300 700',
 });
 
 export const metadata: Metadata = {
@@ -25,7 +41,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${pretendard.variable} font-pretendard antialiased`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,37 +2,63 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 min-h-screen max-w-screen-xl px-20 pb-20 font-[family-name:var(--font-geist-sans)]">
-      <header className="flex flex-col h-[100px] bg-yellow-300 lg:sticky lg:top-0 lg:py-24">
-        <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
-        <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">I'm Hyunyi Kim</h1>
-        <p>Web Developer</p>
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 min-h-screen max-w-screen-xl px-20">
+      <header className="flex flex-col max-h-screen lg:sticky lg:top-0 lg:py-24">
+        <Image className="mb-8" src="/next.svg" alt="Next.js logo" width={180} height={180} priority />
+        <h1 className="text-4xl font-bold tracking-tight sm:text-5xl mb-4">Hyunyi Kim</h1>
+        <p className="text-xl">
+          Web developer with 5 years of experience, <br />
+          driven by curiosity and a love for solving problems.
+        </p>
       </header>
-      <main className="h-[1000px] bg-blue-300">
-        <section className="mb-8 lg:mb-16">
-          <p>I'm a web developer with a passion for creating user-friendly and efficient web applications.</p>
+      <main className="flex flex-col gap-8 lg:gap-16 h-[1000px] lg:py-24">
+        <section className="">
+          <div>
+            <p className="text-lg mb-2">
+              I build fast, reliable, and user-friendly web applications, whether handling high-traffic environments or working with teams in fast-paced
+              settings.
+            </p>
+            <p className="text-lg mb-2">I’m dedicated to finding effective, practical solutions that make a real impact.</p>
+            <p className="text-lg mb-2">
+              In my free time, I love being in nature. I often go jogging and lie on the grass, watching the trees and sky, which gives me the energy to work on
+              my computer. I’m also a huge Korean food lover.
+            </p>
+          </div>
           <div className="flex flex-row">
             <a className="mr-5">Linkedin</a>
             <a className="mr-5">Github</a>
+            <a className="mr-5">Email</a>
             <a className="mr-5">Resume</a>
           </div>
         </section>
-        <section className="mb-8 lg:mb-16">
-          <h3>Working Experience</h3>
+        <section className="">
+          <h3 className="text-2xl font-semibold mb-2">Working Experience</h3>
+          <ul className="flex flex-col gap-2">
+            <li className="flex flex-col gap-1 bg-white p-4 rounded-lg">
+              <div className="flex flex-row justify-between items-center">
+                <span className="font-semibold ">Full stack Engineer, MassAdoption</span>
+                <span className="text-sm">Feb 2024 - Feb 2024</span>
+              </div>
+              <div className="text-sm">Implemented a micro-frontend architecture to support both legacy and new environments</div>
+            </li>
+            <li className="flex flex-col gap-1 bg-white p-4 rounded-lg">
+              <div className="flex flex-row justify-between items-center">
+                <span className="font-semibold ">Full stack Engineer, MassAdoption</span>
+                <span className="text-sm">Feb 2024 - Feb 2024</span>
+              </div>
+              <div className="text-sm">Implemented a micro-frontend architecture to support both legacy and new environments</div>
+            </li>
+          </ul>
+        </section>
+        <section className="">
+          <h3 className="text-2xl font-semibold">Side Projects</h3>
           <ul>
             <li>2024</li>
             <li>2024</li>
           </ul>
         </section>
-        <section className="mb-8 lg:mb-16">
-          <h3>Side Projects</h3>
-          <ul>
-            <li>2024</li>
-            <li>2024</li>
-          </ul>
-        </section>
-        <section className="mb-8 lg:mb-16">
-          <h3>What I like</h3>
+        <section className="">
+          <h3 className="text-2xl font-semibold">What I like</h3>
           <ul>
             <li>2024</li>
             <li>2024</li>
