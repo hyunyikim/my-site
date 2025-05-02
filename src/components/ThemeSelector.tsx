053e702e6314223d3b8007e2410e6430d9ed48a2
Hyunyi Kim <hyunyikim11@gmail.com>
2025-05-02 16:05:58 +0200

    feat: add scroll based show/hide animation

--- src/components/ThemeSelector.tsx
+++ src/components/ThemeSelector.tsx
@@ -2,7 +2,7 @@
 
 import { useTheme } from '@/contexts/theme';
 import { Theme } from '@/types/theme';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { IoSunny, IoMoon, IoSettings } from 'react-icons/io5';
 
@@ -14,16 +14,33 @@ const THEME_LENGTH = THEME_LIST.length;
 
 export default function ThemeSelector() {
   const { selectedTheme, onChangeTheme } = useTheme();
+  const [scrollDirection, setScrollDirection] = useState<'up' | 'down'>('up');
 
   const selectedIndex = THEME_LIST.findIndex((item) => item === selectedTheme);
   const previousIndex = (selectedIndex + 1 + THEME_LENGTH) % THEME_LENGTH;
 
   useEffect(() => {
-    console.log('selectedTheme', selectedTheme);
-  }, [selectedTheme]);
+    let lastScrollY = window.scrollY;
+    const updateScrollDirection = () => {
+      if (window.scrollY > lastScrollY) {
+        setScrollDirection('down');
+      } else {
+        setScrollDirection('up');
+      }
+      lastScrollY = window.scrollY;
+    };
+    window.addEventListener('scroll', updateScrollDirection);
+
+    return () => {
+      window.removeEventListener('scroll', updateScrollDirection);
+    };
+  }, []);
 
   return (
-    <div className="flex flex-row justify-center items-center relative w-20 h-8 overflow-hidden">
+    <div
+      className={`flex flex-row justify-center items-center relative top-8 w-20 h-8 overflow-hidden ${
+        scrollDirection === 'up' ? 'translate-y-0' : '-translate-y-16'
+      } transition-transform duration-200`}>
       {THEME_LIST.map((item, index) => {
         return (
           <button
