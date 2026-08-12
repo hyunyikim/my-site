2761f7d9c3e46c94e826ce1e458a9c72609deaaf
Hyunyi Kim <hyunyikim11@gmail.com>
2026-08-12 12:10:38 +0200

    feat: update content

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -7,33 +7,37 @@ export type WorkExperience = {
 
 export const WORK_EXPERIENCES: WorkExperience[] = [
   {
-    role: 'Frontend Developer',
-    company: 'MassAdoption',
-    period: 'Jul 2025 – Apr 2026',
-    description: 'Built and maintained a large-scale B2C web application used by 200K+ monthly active users',
+    role: "Frontend Engineer",
+    company: "MassAdoption",
+    period: "Jul 2025 – Apr 2026",
+    description:
+      "Built and maintained a B2C web application used by 200K+ monthly active users",
   },
   {
-    role: 'Frontend Developer',
-    company: 'mypaperwork',
-    period: 'Aug 2022 – Apr 2024',
-    description: 'Developed a responsive webpage.',
+    role: "Frontend Engineer",
+    company: "mypaperwork",
+    period: "Aug 2022 – Apr 2024",
+    description: "Built a responsive, interactive webpage.",
   },
   {
-    role: 'Frontend Developer',
-    company: 'MassAdoption',
-    period: 'Aug 2022 – Apr 2024',
-    description: 'Built and maintained a B2B SaaS service, successfully migrating legacy features to a new environment.',
+    role: "Frontend Engineer",
+    company: "MassAdoption",
+    period: "Aug 2022 – Apr 2024",
+    description:
+      "Built and maintained a B2B SaaS service, successfully migrating legacy features to a new environment.",
   },
   {
-    role: 'Full Stack Developer',
-    company: 'OasisBusiness',
-    period: 'Feb 2021 – Aug 2022',
-    description: 'Developed and managed both frontend and backend systems for small business owners.',
+    role: "Full Stack Engineer",
+    company: "OasisBusiness",
+    period: "Feb 2021 – Aug 2022",
+    description:
+      "Developed and managed both frontend and backend systems for small business owners.",
   },
   {
-    role: 'Full Stack Developer',
-    company: 'HanteoGlobal',
-    period: 'Jun 2019 – Dec 2020',
-    description: 'Created and supported frontend and backend functionality for a global K-pop fan community.',
+    role: "Full Stack Engineer",
+    company: "HanteoGlobal",
+    period: "Jun 2019 – Dec 2020",
+    description:
+      "Built and maintained a K-pop big data platform serving 5M+ users worldwide.",
   },
 ];
